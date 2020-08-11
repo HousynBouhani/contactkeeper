8b53fed55b18fd19b51690bec7328657b5bfe243
housyn <housyn123@gmail.com>
2020-08-11 17:37:48 +0100

    db connection,user registration validation on users route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 // @route       POST  api/users
 // @desc        Register a user
 // @access      Public
-router.post('/',(request,response) => {
-    response.send('Register a user');
-});
+router.post(
+  "/",
+  [check("name", "please add a name").not().isEmpty(),
+check('email',"please include a valid email").isEmail(),
+check('password','please enter a password with 6 or more characters')
+.isLength({
+    min:6
+})],
+  (request, response) => {
+    const errors = validationResult(request);
+    if ( !errors.isEmpty()) {
+       return  response.status(400).json({
+            errors:errors.array(),
+        });
+    };
+    response.send("success");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 // bring express with common js
 const express = require('express');
+
+
 //initialize app with express
 const app = express();
-// initialize port 
-const PORT =process.env.PORT || 5000;
-app.listen(PORT,()=> console.log(`server started on ${PORT}...`));
 
+// DB connection 
+const connectDB = require('./config/db');
+connectDB();
+
+
+// accept body request
+app.use(express.json({extended:false}));
+
+
+// index route
+app.get('/',(request,response) => {
+    response.json({"msg":"welcome to the contact keeper app"});
+});
 
 //define routes
 app.use('/api/users',require('./routes/users'));
 app.use('/api/contacts',require('./routes/contacts'));
 app.use('/api/auth',require('./routes/auth'));
 
-// index route
-app.get('/',(request,response) => {
 
-    response.json({"msg":"welcome to the contact keeper app"});
-
-});
-
+// initialize port 
+const PORT =process.env.PORT || 5000;
+app.listen(PORT,()=> console.log(`server started on ${PORT}...`));
